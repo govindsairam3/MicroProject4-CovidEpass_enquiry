08fa3b70181e1aceac6bde207dd3674650e29fe8
govindsairam3 <govindsairam.9@gmail.com>
2021-05-20 23:03:27 +0530

    change in server,login_page

--- server.py
+++ server.py
@@ -13,33 +13,36 @@ app = Flask(__name__, static_url_path='/static')
 def registration_form():
     return render_template('test_page.html')
 
-@app.route('/details', methods=['POST','GET'])
+@app.route('/login_page', methods=['POST','GET'])
 def login_registration_details():
     first_name = request.form['fname']
-    #last_name = request.form['lname']
+    last_name = request.form['lname']
     email_id = request.form['email']
-    source = request.form['Source']
-    destination = request.form['Destination']
-    dest_dist = request.form['Destination-District']
-    contact = request.form['Contact No']
-    #id_proof = request.form['ID']
-    #date = request.form['date_to_GO']
-    fullname = first_name #+'.'+ last_name
+    source_st = request.form['source_state']
+    source_dt = request.form['source']
+    destination_st = request.form['dest_state']
+    destination_dt = request.form['destination']
+    phoneNumber = request.form['phoneNumber']
+    id_proof = request.form['idcard']
+    date = request.form['trip']
+    fullname = first_name +'.'+ last_name
     r = requests.get('https://api.covid19india.org/v4/data.json')
     json_data = r.json()
-    count = json_data[destination]['districts'][dest_dist]['total']['confirmed']
-    population = json_data[destination]['districts'][dest_dist]['meta']['population']
+    count = json_data[destination_st]['districts'][destination_dt]['total']['confirmed']
+    population = json_data[destination_st]['districts'][destination_dt]['meta']['population']
     travel_pass = ((count/population)*100)
     if travel_pass <30 and request.method=='POST' :
         status = 'CONFIRMED'
         #return status
         client.messages.create(to="whatsapp:+917013510927", from_="whatsapp:+14155238886", body="Hello "+fullname+"your travel is "+status)
-        return render_template('login_page.html',var=first_name,var1=email_id,var2=source,var3=destination,var4=dest_dist,var5=contact,var6=status)
+        return render_template('login_page.html',var=fullname, var1=email_id, var2=id_proof, var3=source_st, var4=source_dt,var5=destination_st,var6=destination_dt,
+                                var7=phoneNumber,var8=date, var9=status)
     else:
         status= 'NOT_CONFIRMED'
         #return status
         client.messages.create(to="whatsapp:+917013510927", from_="whatsapp:+14155238886", body="Hello "+fullname+"your travel is "+status)
-        return render_template('login_page.html',var=first_name,var1=email_id,var2=source,var3=destination,var4=dest_dist,var5=contact,var6=status)
+        return render_template('login_page.html',var=fullname, var1=email_id, var2=id_proof, var3=source_st, var4=source_dt,var5=destination_st,var6=destination_dt,
+                                var7=phoneNumber,var8=date, var9=status)
 
 
 if __name__ == "__main__":
